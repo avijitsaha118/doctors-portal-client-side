4227492b4c34868d28899831a2c4666429300f29
avijitsaha118 <sahaavijit908@gmail.com>
2022-05-10 22:33:20 +0600

    review and footer section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
 import Banner from './Banner/Banner';
 import Info from './Info/Info';
 import MakeAppoinment from './MakeAppoinment';
 import Services from './Services';
+import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
@@ -11,6 +13,8 @@ const Home = () => {
             <Info></Info>
             <Services></Services>
             <MakeAppoinment></MakeAppoinment>
+            <Testimonials></Testimonials>
+            <Footer></Footer>
         </div>
     );
 };
