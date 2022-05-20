50bb4cb69965727dd6823329490d0ac876ec01ed
avijitsaha118 <sahaavijit908@gmail.com>
2022-05-20 10:43:59 +0600

    stripe added, error show in UI

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,12 +1,46 @@
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51K26vvDN484WNYDOTppG4gU9ne06nL8pUrmkL8M9y5tpDALMA9CLfD0GHCsmGIFOXcagPUQ4rRJlxLStdADX29ma00RjTcisNq');
 
 const Payment = () => {
-    const {id} = useParams();
+    const { id } = useParams();
+    const url = `http://localhost:5000/booking/${id}`;
+    const { data: appointment, isLoading } = useQuery(['booking', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h2 className='text-2xl bg-purple-700'>Please Comeplete your Payment for {id}</h2>
+
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <p className='text-success'>Hello, {appointment.patientName}</p>
+                    <h2 class="card-title">Please Pay for {appointment.treatment} </h2>
+                    <p>We will see you on <span className='text-orange-400'>{appointment.date}</span> at {appointment.slot}</p>
+                    <p>Please Pay: $ {appointment.price}</p>
+                </div>
+            </div>
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm appointment={appointment}/>
+                    </Elements>
+                </div>
+            </div>
         </div>
+
     );
 };
 
---
+++ src/__payment_steps.js
@@ -0,0 +1,12 @@
+/**
+ * 1. install stripe react
+ * 2. open stripe account
+ * 3. get api key
+ * 4. create element wrapper using publishible key
+ * 5. create checkout form using card elemets
+ * 6. get card elements info(credit card info)
+ * 7. get credit card info/error
+ * 8. get card info and show or display error in UI
+ * 9. get client secret in backend via payment intent
+ * 10. store client secret
+ */
